c6eb5d17b92da3c95ed953e520f2ebc42c15f041
Franck Tameze <francktameze5@gmail.com>
2022-04-01 23:40:11 +0200

    Update GridPrinterImpl.java

--- src/main/java/sudoku/GridPrinterImpl.java
+++ src/main/java/sudoku/GridPrinterImpl.java
@@ -4,5 +4,12 @@ public class GridPrinterImpl implements GridPrinter {
     @Override
     public void print(final Grid grid) {
         // TODO H1: Print grid
+        for (int i = 0; i < 9; i++){
+            for (int x = 0; x < 9; x++){
+                System.out.print(grid.get(i, x) +  "   ");
+            }
+            System.out.println(" ");
+            System.out.println(" ");
+        }
     }
 }
